a642010f7d61b84c85f7dc09adb28e4eaf51fad6
Anderi Setiawan <anderi.setiawan@gmail.com>
2019-02-28 13:09:11 +0700

    Add saldo admin

--- collection/admin.py
+++ collection/admin.py
@@ -1,7 +1,13 @@
 from django.contrib import admin
 
-from .models import ColTarget
+from .models import (
+    ColTarget, Saldo
+)
 
 @admin.register(ColTarget)
 class ColTargetAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(Saldo)
+class SaldoAdmin(admin.ModelAdmin):
     pass
