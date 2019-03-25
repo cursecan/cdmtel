0f94bf2fe7548e3e5e9865130aa3eac62c95168c
Anderi Setiawan <anderi.setiawan@gmail.com>
2019-03-25 10:10:18 +0700

    Create daily running bgtask

--- masterdata/views.py
+++ masterdata/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.http import JsonResponse
 from django.db.models import Count
+from background_task.tasks import Task
+from django.utils import timezone
 
 from .models import Order
 from .tasks import (
@@ -8,20 +10,25 @@ from .tasks import (
     get_record_account
 )
 
+import datetime
+
+today = timezone.now()
+expired_date = today + datetime.timedelta(years=1)
+
 def order_bulk_update_view(request):
     order_obj = Order.objects.filter(closed=False)
     if order_obj.exists():
-        bulk_order_update()
+        bulk_order_update(repeat=50000, repeat_until=expired_date)
         
     return JsonResponse({'process_order': order_obj.count()})
 
 
 def daily_record_view(request):
-    record_data()
+    record_data(repeat=30000, repeat_until=expired_date)
     return JsonResponse({'daily_record': '1'})
 
 
 def record_account_view(request):
-    get_record_account()
+    get_record_account(repeat=50000, repeat_until=expired_date)
     return JsonResponse({'daily_record': '1'})
 
