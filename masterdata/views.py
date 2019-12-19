cfd20477399c9a6d2814de9ebda451ab96e4e2f1
cursecan <anderi.setiawan@gmail.com>
2019-12-19 10:07:56 +0700

    Update scheduling task query

--- masterdata/views.py
+++ masterdata/views.py
@@ -34,6 +34,6 @@ def daily_record_contrak_so(request):
 
 
 def record_account_view(request):
-    get_record_account(repeat=10000, repeat_until=expired_date)
+    get_record_account(repeat=Task.DAILY, repeat_until=expired_date)
     return JsonResponse({'daily_record': '1'})
 
